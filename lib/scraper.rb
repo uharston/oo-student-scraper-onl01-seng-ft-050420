fcda23764ce0d62f149001902840573ac74d0bdc
Uriah Harston <uharston@gmail.com>
2020-05-23 21:16:11 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,23 @@
+#require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    
+  def self.scrape_index_page(index_url) #is responsible for scraping the index page that lists all of the students
+    html = open(index_url)
+    html = open('https://learn-co-curriculum.github.io/student-scraper-test-page/index.html#') #open-uri native method
+    doc = Nokogiri::HTML(html)
+    profiles = doc.css(".card-text-container").text.strip
+    profiles.each {|p| puts p.text.strip}
   end
 
-  def self.scrape_profile_page(profile_url)
-    
+  # .roster-cards-container
+  # .student-card
+
+
+  def self.scrape_profile_page(profile_url) #is responsible for scraping an individual student's profile page to get further information about that student.
+
   end
 
 end
-
