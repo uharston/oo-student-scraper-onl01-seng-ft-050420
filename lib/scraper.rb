c7c476b77e5ae0299455f76140f64eb2a1973c87
Uriah Harston <uharston@gmail.com>
2020-05-23 21:30:28 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,12 +7,13 @@ class Scraper
   def self.scrape_index_page(index_url) #is responsible for scraping the index page that lists all of the students
     # html = open('https://learn-co-curriculum.github.io/student-scraper-test-page/index.html#') #open-uri native method
     doc = Nokogiri::HTML(open(index_url))
-    profiles = doc.css(".card-text-container").text.strip
-    binding.pry
+    profiles = doc.css(".card-text-container .student-name").text.strip
+
     student = Student.new
-    student.name = doc.css("")
-    student.location = doc.css("")
-    student.profile_url = doc.css("")
+    student.name = doc.css(".card-text-container .student-name").text.strip
+    student.location = doc.css(".card-text-container .student-location").text.strip
+    student.profile_url = doc.css(".student-card a")['href']
+      binding.pry
 
 
 
