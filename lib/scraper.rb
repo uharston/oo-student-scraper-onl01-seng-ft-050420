2bd07e36480b2b4da916c8dac9e678ebe2e0c342
Uriah Harston <uharston@gmail.com>
2020-05-23 22:11:16 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
-  #RETURN AN ARRAY OF HASHES 
+  #RETURN AN ARRAY OF HASHES
   def self.scrape_index_page(index_url) #is responsible for scraping the index page that lists all of the students
     doc = Nokogiri::HTML(open(index_url))
     array = []
@@ -12,12 +12,12 @@ class Scraper
       hash[:name] = e.css(".card-text-container .student-name").text.strip
       hash[:location] = e.css(".card-text-container .student-location").text.strip
       hash[:profile_url] = e.css(".student-card a")['href']
-      array << hash 
-    end 
-    array 
-  end 
+      array << hash
+    end
+    array
+  end
 
-  
+
 
 
 
