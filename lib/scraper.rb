bc9e26dfed8332d04d7d108ec56d949e8f03e8a9
Uriah Harston <uharston@gmail.com>
2020-05-23 23:07:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,6 +7,7 @@ class Scraper
   def self.scrape_index_page(index_url) #is responsible for scraping the index page that lists all of the students
     doc = Nokogiri::HTML(open(index_url))
     array = []
+    binding.pry 
     doc.css(".student-card").each do |e|
       hash = {}
       hash[:name] = e.css(".student-name").text
@@ -21,15 +22,15 @@ class Scraper
   def self.scrape_profile_page(profile_url) #is responsible for scraping an individual student's profile page to get further information about that student.
     doc = Nokogiri::HTML(open(profile_url))
     array = []
-    binding.pry
+  
     doc.css(".main-wrapper.profile .vitals-container").each do |e|
       hash = {}
       hash[:twitter] = e.css(".social-icon-container")
       hash[:linkedin] = e.css(".social-icon-container")
       hash[:github] = e.css(".social-icon-container")
       hash[:blog] =
-      hash[:profile_quote] = e.css(".profile-quote")
-      hash[:bio] =
+      hash[:profile_quote] = e.css(".profile-quote").text 
+      hash[:bio] = e.css(".details-container .description-holder p").text 
       array << hash
     end
     array
