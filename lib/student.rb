6d46a7d29bc15e2956a32437b24273ae2e134be9
Uriah Harston <uharston@gmail.com>
2020-05-24 00:20:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,10 +17,10 @@ class Student
   end
 
   def self.create_from_collection(students_array)
- 
+
  students_array.each do |element|
     Student.new(element)
- end 
+ end
   end
 
   def add_student_attributes(attributes_hash)
